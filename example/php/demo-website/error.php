f305dd279cd796598b8e95cf40e7ecfbf64515d4
nulledus <sfuhsjfsfkje@srfsfkssese>
2023-05-11 11:10:29 +0200

    api v2 update

---
+++ example/php/demo-website/error.php
@@ -0,0 +1,17 @@
+<!--
+The user will see this page in case of error.
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My demo website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="container">
+    <h1>Error!</h1>
+    <p>Payment was failed.</p>
+</div>
+</body>
+</html>
